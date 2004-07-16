c6896ed143894103f9c2a68ae8180b43b9b40053
laxlacks <laxlacks>
2004-07-16 03:12:45 +0000

    MQ2FPS => VIP

--- MQ2FPS/MQ2FPS.h
+++
@@ -1,6 +0,0 @@
-//#define FPSINIFile "MQ2FPS.ini"
-
-VOID MaxFPS(PSPAWNINFO pChar, PCHAR szLine);
-VOID SetForegroundMaxFPS(DWORD MaxFPS);
-VOID SetBackgroundMaxFPS(DWORD MaxFPS);
-VOID FPSCommand(PSPAWNINFO pChar, PCHAR szLine);
