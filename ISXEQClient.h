ca1cafdda28301b2e3c10811556182cb63d74e38
brainiac <brainiac2k@gmail.com>
2010-03-31 00:54:31 -0700

    MQ2-20050227a

---
+++ ISXEQClient.h
@@ -0,0 +1,36 @@
+/*****************************************************************************
+    Copyright (C) 2002-2003 Plazmic, 2003-2005 Lax
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+******************************************************************************/
+
+#ifndef _MQ2PLUGIN_H
+#define _MQ2PLUGIN_H
+
+#define MQ2PLUGIN
+#ifndef ISXEQ
+#define ISXEQ
+#endif
+#include "MQ2Main/MQ2Main.h"
+#include "MQ2Main/MQ2Globals.h"
+
+#pragma comment(lib, "ISXEQ")
+
+#ifdef EQLIB_EXPORTS
+#pragma message("EQLIB_EXPORTS")
+#else
+#pragma message("EQLIB_IMPORTS")
+#endif
+
+#define PLUGIN_API extern "C" __declspec(dllexport)
+
+
+
+#endif
