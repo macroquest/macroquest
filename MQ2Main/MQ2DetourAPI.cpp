38340c373195dde18b6ec3a5fe6de5851ea49fa2
brainiac <brainiac2k@gmail.com>
2010-03-31 22:21:37 -0700

    MQ2-20050702b

--- MQ2Main/MQ2DetourAPI.cpp
+++ MQ2Main/MQ2DetourAPI.cpp
@@ -195,7 +195,7 @@ DETOUR_TRAMPOLINE_EMPTY(VOID memchecks_tramp(PVOID,DWORD,PCHAR,DWORD,BOOL));
 
 VOID memchecks(PVOID A,DWORD B,PCHAR C,DWORD D,BOOL E)
 {
-	if (B==0x43C2)
+	if (B==0x55bc)
 	{
 		int Pos = 4 + strlen(&C[4])+ 1;
 		int End = Pos + (int)(71.0*rand()/(RAND_MAX+1.0));
@@ -209,9 +209,9 @@ VOID memchecks(PVOID A,DWORD B,PCHAR C,DWORD D,BOOL E)
 			C[Pos]=(t <= 255) ? (char)t : 0;
 		}
 	}
-	if (B==0x7E1A) PluginsBeginZone(); 
+	if (B==0x2baf) PluginsBeginZone(); 
 	memchecks_tramp(A,B,C,D,E);
-	if (B==0x16EE) PluginsEndZone();
+	if (B==0x5e20) PluginsEndZone();
 }
 
 // ***************************************************************************
