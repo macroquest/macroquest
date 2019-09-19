670d9e40c71c89903e12731e6080c7a0c2999d59
brainiac <brainiac2k@gmail.com>
2019-09-18 21:17:17 -0700

    Friendly error message if MQ2Auth.h is missing

--- src/main/MQ2Auth.cpp
+++ src/main/MQ2Auth.cpp
@@ -23,4 +23,9 @@ using FNCB = DWORD(*)(DWORD, HINSTANCE, DWORD&);
 
 #undef  MQ2AUTH
 #define MQ2AUTH(z) EQLIB_API void z(DWORD x){FNCB f=(FNCB)x;f((DWORD)proc,ghInstance,gh);}
+
+#if __has_include("../../MQ2Auth.h")
 #include "../../MQ2Auth.h"
+#else
+#error "Missing MQ2Auth.h - be sure to run MQ2Auth to generate it"
+#endif
