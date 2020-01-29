bae207458d453361ccfbb408d0e17a76500ed27b
Dustin Niehoff <dannuic@gmail.com>
2020-01-29 08:42:20 -0700

    Implemented move semantics and general efficiency update

--- src/main/MQ2Anonymize.cpp
+++ src/main/MQ2Anonymize.cpp
@@ -12,56 +12,64 @@
  * GNU General Public License for more details.
  */
 
-#include <regex>
-#include <utility>
-
 #include "pch.h"
 #include "MQ2Main.h"
 
+#include <regex>
+#include <utility>
+
 namespace mq {
 
-static class anon_replacer {
+class anon_replacer {
 public:
-	const std::string target;
-	const ci_unordered::set alternates;
-	const std::regex search_string;
+	std::string target;
+	ci_unordered::set alternates;
+	std::regex search_string;
 
 private:
-
-	static std::regex build_regex(std::string_view Name, const ci_unordered::set& Alternates) {
+	std::regex build_regex(std::string_view name)
+	{
 		return std::regex(
-			fmt::format("\b({}{})\b", Name, std::accumulate(Alternates.cbegin(), Alternates.cend(), std::string(),
-				[](const std::string& _text, const std::string& alt) -> std::string {
-					return _text + "|" + alt;
+			fmt::format("\b({}{})\b", name, std::accumulate(alternates.cbegin(), alternates.cend(), std::string(),
+				[](const std::string& text, std::string_view alt) -> std::string {
+					return fmt::format("{}|{}", text, alt);
 				})),
 			std::regex_constants::icase);
 	}
 
-	static ci_unordered::set add_alternate(std::string_view _alternate, ci_unordered::set _alternates) {
-		_alternates.emplace(_alternate);
-		return _alternates;
+	static ci_unordered::set add_alternate(std::string_view alternate, ci_unordered::set&& alternates)
+	{
+		alternates.emplace(alternate);
+		return alternates;
 	}
 
-	static ci_unordered::set drop_alternate(std::string_view _alternate, ci_unordered::set _alternates) {
-		_alternates.erase(_alternate);
-		_alternates;
+	static ci_unordered::set drop_alternate(std::string_view alternate, ci_unordered::set&& alternates)
+	{
+		alternates.erase(alternate);
+		return alternates;
 	}
 
 public:
-	anon_replacer(std::string_view _name, std::string_view _target, ci_unordered::set _alternates)
-		: target(_target), alternates(_alternates), search_string(build_regex(_name, _alternates)) {}
-
-	anon_replacer(const anon_replacer& other)
-		: target(other.target), alternates(other.alternates), search_string(other.search_string) {}
-
-	anon_replacer& operator=(const anon_replacer& other) { return anon_replacer(other); }
-
-	static anon_replacer with_alternate(std::string_view _name, std::string_view _alternate, const anon_replacer& replacer) {
-		return anon_replacer(_name, replacer.target, add_alternate(_alternate, replacer.alternates));
+	anon_replacer(std::string_view name, std::string_view target, ci_unordered::set&& alternates)
+		: target(target), alternates(std::move(alternates))
+	{
+		search_string = build_regex(name);
 	}
 
-	static anon_replacer without_alternate(std::string_view _name, std::string_view _alternate, const anon_replacer& replacer) {
-		return anon_replacer(_name, replacer.target, drop_alternate(_alternate, replacer.alternates));
+	static anon_replacer with_alternate(std::string_view name, std::string_view alternate, anon_replacer&& replacer)
+	{
+		return anon_replacer(name, replacer.target, add_alternate(alternate, std::move(replacer.alternates)));
+	}
+
+	static anon_replacer without_alternate(std::string_view name, std::string_view alternate, anon_replacer&& replacer)
+	{
+		return anon_replacer(name, replacer.target, drop_alternate(alternate, std::move(replacer.alternates)));
+	}
+
+	// this is designed to be used with an accumulate that returns a string, so move the old text in
+	std::string replace_text(std::string&& text)
+	{
+		return std::regex_replace(std::move(text), search_string, ModifyMacroString(target));
 	}
 };
 
@@ -72,31 +80,44 @@ ci_unordered::map<anon_replacer> filter_map;
 // the source string_view here will be used to index
 // creating a regex that looks like `(source|all|the|alternates)`
 
-// add an anonymization rule to the map -- if the rule already exists, assume we want to updated the target
-void AddAnonymization(std::string_view Name, std::string_view Replace) {
+// add an anonymization rule to the map -- if the rule already exists, assume we want to update the target
+void AddAnonymization(std::string_view Name, std::string_view Replace)
+{
 	auto [it, result] = filter_map.try_emplace(Name, anon_replacer(Name, Replace, {}));
 
-	if (!result)
-		it->second = anon_replacer(Name, Replace, it->second.alternates);
+	if (!result) // this is automatically an assign since result is false
+		filter_map.insert_or_assign(Name, anon_replacer(Name, Replace, std::move(it->second.alternates)));
 }
 
-void DropAnonymization(std::string_view Name) {
+void DropAnonymization(std::string_view Name)
+{
 	filter_map.erase(Name);
 }
 
-void AddAlternate(std::string_view Name, std::string_view Alternate) {
-	filter_map[Name] = anon_replacer::with_alternate(Name, Alternate, filter_map[Name]);
+// will not add an entry if there is nothing available
+void AddAlternate(std::string_view Name, std::string_view Alternate)
+{
+	auto result = filter_map.find(Name);
+	// automatically an assign, but we want to make sure not to add an element without a target
+	if (result != filter_map.end())
+		filter_map.insert_or_assign(Name, anon_replacer::with_alternate(Name, Alternate, std::move(result->second)));
 }
 
-void DropAlternate(std::string_view Name, std::string_view Alternate) {
-	filter_map[Name] = anon_replacer::without_alternate(Name, Alternate, filter_map[Name]);
+// will not add an entry if there is nothing available
+void DropAlternate(std::string_view Name, std::string_view Alternate)
+{
+	auto result = filter_map.find(Name);
+	// automatically an assign, but we want to make sure not to add an element without a target
+	if (result != filter_map.end())
+		filter_map.insert_or_assign(Name, anon_replacer::without_alternate(Name, Alternate, std::move(result->second)));
 }
 
 // process string to anonymize
-std::string Anonymize(std::string_view Text) {
+std::string Anonymize(std::string_view Text)
+{
 	return std::accumulate(filter_map.cbegin(), filter_map.cend(), std::string(Text),
-		[](const std::string& _text, std::pair<std::string_view, anon_replacer> _pair) -> std::string {
-			return std::regex_replace(_text, _pair.second.search_string, ModifyMacroString(_pair.second.target));
+		[](std::string _text, std::pair<std::string_view, anon_replacer> _pair) -> std::string {
+			return _pair.second.replace_text(std::move(_text));
 		});
 }
 }
