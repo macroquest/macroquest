71910865a60ac0e8c135f17cf872cc7a44511721
Dustin Niehoff <dannuic@gmail.com>
2020-02-03 22:48:19 -0700

    Updated arg parsing and implemented it in the mqanon command

--- src/main/MQ2CommandAPI.cpp
+++ src/main/MQ2CommandAPI.cpp
@@ -902,6 +902,7 @@ void InitializeMQ2Commands()
 		{ "/quit",              QuitCmd,                    true,  false },
 		{ "/removelev",         RemoveLevCmd,               true,  false },
 		{ "/engine",            EngineCommand,              true,  false },
+		{ "/mqanon",            MQAnon,                     false, false },
 
 		{ nullptr,              nullptr,                    false, true  },
 	};
