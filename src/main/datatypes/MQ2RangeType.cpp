c4f6a0040a1f9eb2a5538b3b50afe832cd742656
Knightly <knightly.cavalier@gmail.com>
2020-02-04 16:59:55 -1000

    Change Range to allow for testing int64

--- src/main/datatypes/MQ2RangeType.cpp
+++ src/main/datatypes/MQ2RangeType.cpp
@@ -26,9 +26,9 @@ bool MQ2RangeType::GetMember(MQVarPtr VarPtr, char* Member, char* Index, MQTypeV
 	if (!Index[0])
 		return false;
 
-	int P1 = 0;
-	int P2 = 0;
-	int P3 = 0;
+	int64_t P1 = 0;
+	int64_t P2 = 0;
+	int64_t P3 = 0;
 
 	switch (static_cast<RangeMembers>(pMember->ID))
 	{
@@ -40,12 +40,12 @@ bool MQ2RangeType::GetMember(MQVarPtr VarPtr, char* Member, char* Index, MQTypeV
 			if (char* pColon = strchr(Index, ':'))
 			{
 				*pColon = 0;
-				P3 = GetIntFromString(&pColon[1], P3);
+				P3 = GetInt64FromString(&pColon[1], P3);
 				if (char* pComma = strchr(Index, ','))
 				{
 					*pComma = 0;
-					P2 = GetIntFromString(&pComma[1], P2);
-					P1 = GetIntFromString(Index, P1);
+					P2 = GetInt64FromString(&pComma[1], P2);
+					P1 = GetInt64FromString(Index, P1);
 
 					if (P3 > P1 && P3 < P2)
 					{
@@ -64,12 +64,12 @@ bool MQ2RangeType::GetMember(MQVarPtr VarPtr, char* Member, char* Index, MQTypeV
 			if (char* pColon = strchr(Index, ':'))
 			{
 				*pColon = 0;
-				P3 = GetIntFromString(&pColon[1], P3);
+				P3 = GetInt64FromString(&pColon[1], P3);
 				if (char* pComma = strchr(Index, ','))
 				{
 					*pComma = 0;
-					P2 = GetIntFromString(&pComma[1], P2);
-					P1 = GetIntFromString(Index, P1);
+					P2 = GetInt64FromString(&pComma[1], P2);
+					P1 = GetInt64FromString(Index, P1);
 
 					if (P3 >= P1 && P3 <= P2)
 					{
