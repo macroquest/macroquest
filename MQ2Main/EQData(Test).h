764ca4ed997455f9e8fdc9c921da522f343c8cd6
brainiac <brainiac2k@gmail.com>
2020-02-08 05:47:18 +0000

    MQ2-20200208(Test)

--- MQ2Main/EQData(Test).h
+++ MQ2Main/EQData(Test).h
@@ -5138,7 +5138,7 @@ typedef struct _GROUPAGGRO {
 } GROUPAGGRO, *PGROUPAGGRO;
 
 #define EQ_ASSIST_CALC                  0x020c0f19
-#define EQ_ASSIST_COMPLETE              0x1547   // aMsgTimeStampRe
+#define EQ_ASSIST_COMPLETE              0x5f34   // aMsgTimeStampRe
 #define EQ_ASSIST                       0x6ED3   // do_assist(PlayerClient *,char const *)+399 20160212 live (see 52C319)
 #define EQ_LoadingS__ArraySize          0x5a     // EQ_LoadingS__SetProgressBar_x+76 	(4C7396 yes it says 5b there, but we dont want to overwrite the NULL term...	2016 Apr 21
 };
