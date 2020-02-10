4cee1cffcabea23a38b42b2f2d258ab5e9dc30a8
Knightly <knightly.cavalier@gmail.com>
2020-02-10 06:35:59 -1000

    Update Plugin Template

--- tools/plugin_template/MQ2Template.cpp
+++ tools/plugin_template/MQ2Template.cpp
@@ -3,142 +3,382 @@
 
 // PLUGIN_API is only to be used for callbacks.  All existing callbacks at this time
 // are shown below. Remove the ones your plugin does not use.  Always use Initialize
-// and Shutdown for setup and cleanup, do NOT do it in DllMain.
+// and Shutdown for setup and cleanup.
 
-
-
-#include "../MQ2Plugin.h"
-
+#include <mq/Plugin.h>
 
 PreSetup("MQ2Template");
+PLUGIN_VERSION(0.1);
 
-// Called once, when the plugin is to initialize
+/**
+ * @fn InitializePlugin
+ *
+ * This is called once on plugin initialization and can be considered the startup
+ * routine for the plugin.
+ */
 PLUGIN_API void InitializePlugin()
 {
-    DebugSpewAlways("Initializing MQ2Template");
+	DebugSpewAlways("MQ2Template::Initializing version %f", MQ2Version);
 
-    //Add commands, MQ2Data items, hooks, etc.
-    //AddCommand("/mycommand",MyCommand);
-    //AddXMLFile("MQUI_MyXMLFile.xml");
-    //bmMyBenchmark=AddMQ2Benchmark("My Benchmark Name");
+	// Examples:
+	// AddCommand("/mycommand", MyCommand);
+	// AddXMLFile("MQUI_MyXMLFile.xml");
+	// AddMQ2Data("mytlo", MyTLOData);
 }
 
-// Called once, when the plugin is to shutdown
+/**
+ * @fn ShutdownPlugin
+ *
+ * This is called once when the plugin has been asked to shutdown.  The plugin has
+ * not actually shut down until this completes.
+ */
 PLUGIN_API void ShutdownPlugin()
 {
-    DebugSpewAlways("Shutting down MQ2Template");
+	DebugSpewAlways("MQ2Template::Shutting down");
 
-    //Remove commands, MQ2Data items, hooks, etc.
-    //RemoveMQ2Benchmark(bmMyBenchmark);
-    //RemoveCommand("/mycommand");
-    //RemoveXMLFile("MQUI_MyXMLFile.xml");
+	// Examples:
+	// RemoveCommand("/mycommand");
+	// RemoveXMLFile("MQUI_MyXMLFile.xml");
+	// RemoveMQ2Data("mytlo");
 }
 
-// Called after entering a new zone
-PLUGIN_API void OnZoned()
-{
-    DebugSpewAlways("MQ2Template::OnZoned()");
-}
-
-// Called once directly before shutdown of the new ui system, and also
-// every time the game calls CDisplay::CleanGameUI()
+/**
+ * @fn OnCleanUI
+ *
+ * This is called once just before the shutdown of the UI system and each time the
+ * game requests that the UI be cleaned.  Most commonly this happens when a
+ * /loadskin command is issued, but it also occurs when reaching the character
+ * select screen and when first entering the game.
+ *
+ * One purpose of this function is to allow you to destroy any custom windows that
+ * you have created and cleanup any UI items that need to be removed.
+ */
 PLUGIN_API void OnCleanUI()
 {
-    DebugSpewAlways("MQ2Template::OnCleanUI()");
-    // destroy custom windows, etc
+	// DebugSpewAlways("MQ2Template::OnCleanUI()");
 }
 
-// Called once directly after the game ui is reloaded, after issuing /loadskin
+/**
+ * @fn OnReloadUI
+ *
+ * This is called once just after the UI system is loaded. Most commonly this
+ * happens when a /loadskin command is issued, but it also occurs when first
+ * entering the game.
+ *
+ * One purpose of this function is to allow you to recreate any custom windows
+ * that you have setup.
+ */
 PLUGIN_API void OnReloadUI()
 {
-    DebugSpewAlways("MQ2Template::OnReloadUI()");
-    // recreate custom windows, etc
+	// DebugSpewAlways("MQ2Template::OnReloadUI()");
 }
 
-// Called every frame that the "HUD" is drawn -- e.g. net status / packet loss bar
+/**
+ * @fn OnDrawHUD
+ *
+ * This is called each time the Heads Up Display (HUD) is drawn.  The HUD is
+ * responsible for the net status and packet loss bar.
+ *
+ * Note that this is not called at all if the HUD is not shown (default F11 to
+ * toggle).
+ *
+ * Because the net status is updated frequently, it is recommended to have a
+ * timer or counter at the start of this call to limit the amount of times the
+ * code in this section is executed.
+ */
 PLUGIN_API void OnDrawHUD()
 {
-    // DONT leave in this debugspew, even if you leave in all the others
-    //DebugSpewAlways("MQ2Template::OnDrawHUD()");
+/*
+	static int DrawHUDCount = 0;
+	// Skip ~500 draws
+	if (++DrawHUDCount > 500)
+	{
+		DrawHUDCount = 0;
+		DebugSpewAlways("MQ2Template::OnDrawHUD()");
+	}
+*/
 }
 
-// Called once directly after initialization, and then every time the gamestate changes
-PLUGIN_API void SetGameState(DWORD GameState)
+/**
+ * @fn SetGameState
+ *
+ * This is called when the GameState changes.  It is also called once after the
+ * plugin is initialized.
+ *
+ * For a list of known GameState values, see the constants that begin with
+ * GAMESTATE_.  The most commonly used of these is GAMESTATE_INGAME.
+ *
+ * When zoning, this is called once after @ref OnBeginZone @ref OnRemoveSpawn
+ * and @ref OnRemoveGroundItem are all done and then called once again after
+ * @ref OnEndZone and @ref OnAddSpawn are done but prior to @ref OnAddGroundItem
+ * and @ref OnZoned
+ *
+ * @param GameState int - The value of GameState at the time of the call
+ */
+PLUGIN_API void SetGameState(int GameState)
 {
-    DebugSpewAlways("MQ2Template::SetGameState()");
-    //if (GameState==GAMESTATE_INGAME)
-    // create custom windows if theyre not set up, etc
+	// DebugSpewAlways("MQ2Template::SetGameState(%d)", GameState);
 }
 
 
-// This is called every time MQ pulses
+/**
+ * @fn OnPulse
+ *
+ * This is called each time MQ2 goes through its heartbeat (pulse) function.
+ *
+ * Because this happens very frequently, it is recommended to have a timer or
+ * counter at the start of this call to limit the amount of times the code in
+ * this section is executed.
+ */
 PLUGIN_API void OnPulse()
 {
-    // DONT leave in this debugspew, even if you leave in all the others
-    //DebugSpewAlways("MQ2Template::OnPulse()");
+/*
+	static int PulseCount = 0;
+	// Skip ~500 pulses
+	if (++PulseCount > 500)
+	{
+		PulseCount = 0;
+		DebugSpewAlways("MQ2Template::OnPulse()");
+	}
+*/
 }
 
-// This is called every time WriteChatColor is called by MQ2Main or any plugin,
-// IGNORING FILTERS, IF YOU NEED THEM MAKE SURE TO IMPLEMENT THEM. IF YOU DONT
-// CALL CEverQuest::dsp_chat MAKE SURE TO IMPLEMENT EVENTS HERE (for chat plugins)
-PLUGIN_API DWORD OnWriteChatColor(char* Line, DWORD Color, DWORD Filter)
+/**
+ * @fn OnWriteChatColor
+ *
+ * This is called each time WriteChatColor is called (whether by MQ2Main or by any
+ * plugin).  This can be considered the "when outputting text from MQ" callback.
+ *
+ * This ignores filters on display, so if they are needed either implement them in
+ * this section or see @ref OnIncomingChat where filters are already handled.
+ *
+ * If CEverQuest::dsp_chat is not called, and events are required, they'll need to
+ * be implemented here as well.  Otherwise, see @ref OnIncomingChat where that is
+ * already handled.
+ *
+ * For a list of Color values, see the constants for USERCOLOR_.  The default is
+ * USERCOLOR_DEFAULT.
+ *
+ * @param Line const char* - The line that was passed to WriteChatColor
+ * @param Color int - The type of chat text this is to be sent as
+ * @param Filter int - (default 0)
+ */
+PLUGIN_API void OnWriteChatColor(const char* Line, int Color, int Filter)
 {
-    DebugSpewAlways("MQ2Template::OnWriteChatColor(%s)",Line);
-    return 0;
+	// DebugSpewAlways("MQ2Template::OnWriteChatColor(%s, %d, %d)", Line, Color, Filter);
 }
 
-// This is called every time EQ shows a line of chat with CEverQuest::dsp_chat,
-// but after MQ filters and chat events are taken care of.
-PLUGIN_API DWORD OnIncomingChat(char* Line, DWORD Color)
+/**
+ * @fn OnIncomingChat
+ *
+ * This is called each time a line of chat is shown.  It occurs after MQ filters
+ * and chat events have been handled.  If you need to know when MQ2 has sent chat,
+ * consider using @ref OnWriteChatColor instead.
+ *
+ * For a list of Color values, see the constants for USERCOLOR_. The default is
+ * USERCOLOR_DEFAULT.
+ *
+ * @param Line const char* - The line of text that was shown
+ * @param Color int - The type of chat text this was sent as
+ *
+ * @return bool - whether something was done based on the incoming chat
+ */
+PLUGIN_API bool OnIncomingChat(const char* Line, DWORD Color)
 {
-    DebugSpewAlways("MQ2Template::OnIncomingChat(%s)",Line);
-    return 0;
+	// DebugSpewAlways("MQ2Template::OnIncomingChat(%s, %d)", Line, Color);
+	return false;
 }
 
-// This is called each time a spawn is added to a zone (inserted into EQ's list of spawns),
-// or for each existing spawn when a plugin first initializes
-// NOTE: When you zone, these will come BEFORE OnZoned
+/**
+ * @fn OnAddSpawn
+ *
+ * This is called each time a spawn is added to a zone (ie, something spawns). It is
+ * also called for each existing spawn when a plugin first initializes.
+ *
+ * When zoning, this is called for all spawns in the zone after @ref OnEndZone is
+ * called and before @ref OnZoned is called.
+ *
+ * @param pNewSpawn PSPAWNINFO - The spawn that was added
+ */
 PLUGIN_API void OnAddSpawn(PSPAWNINFO pNewSpawn)
 {
-    DebugSpewAlways("MQ2Template::OnAddSpawn(%s)",pNewSpawn->Name);
+	// DebugSpewAlways("MQ2Template::OnAddSpawn(%s)", pNewSpawn->Name);
 }
 
-// This is called each time a spawn is removed from a zone (removed from EQ's list of spawns).
-// It is NOT called for each existing spawn when a plugin shuts down.
+/**
+ * @fn OnRemoveSpawn
+ *
+ * This is called each time a spawn is removed from a zone (ie, something despawns
+ * or is killed).  It is NOT called when a plugin shuts down.
+ *
+ * When zoning, this is called for all spawns in the zone after @ref OnBeginZone is
+ * called.
+ *
+ * @param pSpawn PSPAWNINFO - The spawn that was removed
+ */
 PLUGIN_API void OnRemoveSpawn(PSPAWNINFO pSpawn)
 {
-    DebugSpewAlways("MQ2Template::OnRemoveSpawn(%s)",pSpawn->Name);
+	// DebugSpewAlways("MQ2Template::OnRemoveSpawn(%s)", pSpawn->Name);
 }
 
-// This is called each time a ground item is added to a zone
-// or for each existing ground item when a plugin first initializes
-// NOTE: When you zone, these will come BEFORE OnZoned
+/**
+ * @fn OnAddGroundItem
+ *
+ * This is called each time a ground item is added to a zone (ie, something spawns).
+ * It is also called for each existing ground item when a plugin first initializes.
+ *
+ * When zoning, this is called for all ground items in the zone after @ref OnEndZone
+ * is called and before @ref OnZoned is called.
+ *
+ * @param pNewGroundItem PGROUNDITEM - The ground item that was added
+ */
 PLUGIN_API void OnAddGroundItem(PGROUNDITEM pNewGroundItem)
 {
-    DebugSpewAlways("MQ2Template::OnAddGroundItem(%d)",pNewGroundItem->DropID);
+	// DebugSpewAlways("MQ2Template::OnAddGroundItem(%d)", pNewGroundItem->DropID);
 }
 
-// This is called each time a ground item is removed from a zone
-// It is NOT called for each existing ground item when a plugin shuts down.
+/**
+ * @fn OnRemoveGroundItem
+ *
+ * This is called each time a ground item is removed from a zone (ie, something
+ * despawns or is picked up).  It is NOT called when a plugin shuts down.
+ *
+ * When zoning, this is called for all ground items in the zone after
+ * @ref OnBeginZone is called.
+ *
+ * @param pGroundItem PGROUNDITEM - The ground item that was removed
+ */
 PLUGIN_API void OnRemoveGroundItem(PGROUNDITEM pGroundItem)
 {
-    DebugSpewAlways("MQ2Template::OnRemoveGroundItem(%d)",pGroundItem->DropID);
+	// DebugSpewAlways("MQ2Template::OnRemoveGroundItem(%d)", pGroundItem->DropID);
 }
 
-// This is called when we receive the EQ_BEGIN_ZONE packet is received
+/**
+ * @fn OnBeginZone
+ *
+ * This is called just after entering a zone line and as the loading screen appears.
+ */
 PLUGIN_API void OnBeginZone()
 {
-    DebugSpewAlways("MQ2Template::OnBeginZone");
+	// DebugSpewAlways("MQ2Template::OnBeginZone()");
 }
 
-// This is called when we receive the EQ_END_ZONE packet is received
+/**
+ * @fn OnEndZone
+ *
+ * This is called just after the loading screen, but prior to the zone being fully
+ * loaded.
+ *
+ * This should occur before @ref OnAddSpawn and @ref OnAddGroundItem are called. It
+ * always occurs before @ref OnZoned is called.
+ */
 PLUGIN_API void OnEndZone()
 {
-    DebugSpewAlways("MQ2Template::OnEndZone");
+	// DebugSpewAlways("MQ2Template::OnEndZone()");
 }
-// This is called when pChar!=pCharOld && We are NOT zoning
-// honestly I have no idea if its better to use this one or EndZone (above)
-PLUGIN_API void Zoned()
+
+/**
+ * @fn OnZoned
+ *
+ * This is called after entering a new zone and the zone is considered "loaded."
+ *
+ * It occurs after @ref OnEndZone @ref OnAddSpawn and @ref OnAddGroundItem have
+ * been called.
+ */
+PLUGIN_API void OnZoned()
 {
-    DebugSpewAlways("MQ2Template::Zoned");
+	// DebugSpewAlways("MQ2Template::OnZoned()");
+}
+
+/**
+ * @fn OnUpdateImGui
+ *
+ * This is called each time that the ImGui Overlay is rendered. Use this to render
+ * and update plugin specific widgets.
+ *
+ * Because this happens extremely frequently, it is recommended to move any actual
+ * work to a separate call and use this only for updating the display.
+ */
+PLUGIN_API void OnUpdateImGui()
+{
+/*
+	static int UpdateCount = 0;
+	// Skip ~4000 updates for debug spew message
+	if (++UpdateCount > 4000)
+	{
+		UpdateCount = 0;
+		DebugSpewAlways("MQ2Template::OnUpdateImGui()");
+	}
+
+	if (GetGameState() == GAMESTATE_INGAME)
+	{
+		static bool ShowMQ2TemplateWindow = true;
+		ImGui::Begin("MQ2Template", &ShowMQ2TemplateWindow, ImGuiWindowFlags_MenuBar);
+		if (ImGui::BeginMenuBar())
+		{
+			ImGui::Text("MQ2Template is loaded!");
+			ImGui::EndMenuBar();
+		}
+		ImGui::End();
+	}
+*/
+}
+
+/**
+ * @fn OnMacroStart
+ *
+ * This is called each time a macro starts (ex: /mac somemacro.mac), prior to
+ * launching the macro.
+ *
+ * @param Name const char* - The name of the macro that was launched
+ */
+PLUGIN_API void OnMacroStart(const char* Name)
+{
+	// DebugSpewAlways("MQ2Template::OnMacroStart(%s)", Name);
+}
+
+/**
+ * @fn OnMacroStop
+ *
+ * This is called each time a macro stops (ex: /endmac), after the macro has ended.
+ *
+ * @param Name const char* - The name of the macro that was stopped.
+ */
+PLUGIN_API void OnMacroStop(const char* Name)
+{
+	// DebugSpewAlways("MQ2Template::OnMacroStop(%s)", Name);
+}
+
+/**
+ * @fn OnLoadPlugin
+ *
+ * This is called each time a plugin is loaded (ex: /plugin someplugin), after the
+ * plugin has been loaded and any associated -AutoExec.cfg file has been launched.
+ * This means it will be executed after the plugin's @ref InitializePlugin callback.
+ *
+ * This is also called when THIS plugin is loaded, but initialization tasks should
+ * still be done in @ref InitializePlugin.
+ *
+ * @param Name const char* - The name of the plugin that was loaded
+ */
+PLUGIN_API void OnLoadPlugin(const char* Name)
+{
+	// DebugSpewAlways("MQ2Template::OnLoadPlugin(%s)", Name);
+}
+
+/**
+ * @fn OnUnloadPlugin
+ *
+ * This is called each time a plugin is unloaded (ex: /plugin someplugin unload),
+ * just prior to the plugin unloading.  This means it will be executed prior to that
+ * plugin's @ref ShutdownPlugin callback.
+ *
+ * This is also called when THIS plugin is unloaded, but shutdown tasks should still
+ * be done in @ref ShutdownPlugin.
+ *
+ * @param Name const char* - The name of the plugin that is to be unloaded
+ */
+PLUGIN_API void OnUnloadPlugin(const char* Name)
+{
+	// DebugSpewAlways("MQ2Template::OnUnloadPlugin(%s)", Name);
 }
