f985e4f47925da94d1dfd2e04d1acd33e486c916
Knightly <knightly.cavalier@gmail.com>
2020-05-17 06:47:00 -1000

    Add Plugin Versioning script to build_scripts

---
+++ tools/build_scripts/Plugin_Versioning.ps1
@@ -0,0 +1,32 @@
+Param (
+    [Parameter(
+        Mandatory = $true,
+        ValueFromPipeLine = $true,
+        ValueFromPipelineByPropertyName = $true
+    )]
+    [string]$ProjectName,
+
+    [Parameter(
+        Mandatory = $true,
+        ValueFromPipeLine = $true,
+        ValueFromPipelineByPropertyName = $true
+    )]
+    [string]$ProjectDirectory
+)
+
+$CPPFile = Get-ChildItem -Path $ProjectDirectory -Recurse -Filter "$ProjectName.cpp"
+$ResFile = Get-ChildItem -Path $ProjectDirectory -Recurse -Filter "$ProjectName.rc"
+if ($CPPFile.count -eq 1 -and $ResFile.count -eq 1) {
+    $VersionMatch = Select-String -Path $CPPFile -Pattern 'PLUGIN_VERSION\((.*)\)'
+    $Version = $VersionMatch.Matches.Groups[1].Value
+
+    If ($Version) {
+        $VersionCommas = $Version -replace '\.', ','
+        (Get-Content $ResFile) | ForEach-Object {
+            $_ -replace 'FILEVERSION (.*)', "FILEVERSION $VersionCommas" `
+            -replace 'PRODUCTVERSION (.*)', "PRODUCTVERSION $VersionCommas" `
+            -replace 'VALUE "FileVersion", (.*)', "VALUE `"FileVersion`", `"$Version`"" `
+            -replace 'VALUE "ProductVersion", (.*)', "VALUE `"ProductVersion`", `"$Version`""
+        } | Set-Content $ResFile
+    }
+}
