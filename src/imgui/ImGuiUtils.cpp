7cef42ff4c123fef6e414aa9cbdfdb1b4c02e0c8
brainiac <brainiac2k@gmail.com>
2021-01-24 23:41:31 -0800

    Update copyright date

--- src/imgui/ImGuiUtils.cpp
+++ src/imgui/ImGuiUtils.cpp
@@ -1,6 +1,6 @@
 /*
  * MacroQuest: The extension platform for EverQuest
- * Copyright (C) 2002-2020 MacroQuest Authors
+ * Copyright (C) 2002-2021 MacroQuest Authors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
