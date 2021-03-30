739c7da519db51208bbd18ff2a71e18fc59afc41
Dustin Niehoff <dannuic@gmail.com>
2021-03-30 15:14:24 -0600

    Added some lua examples

---
+++ data/lua/examples/autoassist.lua
@@ -0,0 +1,50 @@
+mq = require('mq')
+
+local GroupAssist = function()
+    local currentTargetID = mq.TLO.Target.ID()
+    local groupAssist = function () return mq.TLO.Me.GroupAssistTarget end -- Note the lack of parentheses here
+    local assistHPs = groupAssist().PctHPs()
+    if currentTargetID ~= groupAssist().ID() and assistHPs ~= nil and assistHPs <= 98 then
+        print('Targeting '..groupAssist().DisplayName())
+        groupAssist().DoTarget()
+    end
+end
+
+local openGUI = true
+local shouldDrawGUI = true
+
+local AssistGUI = function()
+    if shouldDrawGUI then
+        openGUI, shouldDrawGUI = ImGui.Begin('Assist GUI', openGUI)
+
+        local pctHPs = mq.TLO.Me.GroupAssistTarget.PctHPs()
+        if not pctHPs then pctHPs = 0 end
+        local ratioHPs = pctHPs / 100
+
+        local targetName = mq.TLO.Me.GroupAssistTarget.DisplayName()
+        if not targetName then targetName = 'No Target' end
+
+        ImGui.PushStyleColor(ImGuiCol.PlotHistogram, 1 - ratioHPs, ratioHPs, 0, 1)
+        ImGui.PushStyleColor(ImGuiCol.Text, 0.5, 0.5, 0.5, 1)
+        ImGui.ProgressBar(ratioHPs, -1, -1, targetName..': '..pctHPs..'%')
+        ImGui.PopStyleColor(2)
+
+        ImGui.End()
+    end
+end
+
+mq.imgui.init('assistGUI', AssistGUI)
+mq.bind('/assgui', function()
+    openGUI = not openGUI
+    shouldDrawGUI = openGUI
+end)
+
+local terminate = false
+mq.bind('/assend', function() terminate = true end)
+
+while not terminate do
+    GroupAssist()
+    --mq.delay('1s')
+    mq.delay(1000)
+    print('test')
+end
---
+++ data/lua/examples/fish.lua
@@ -0,0 +1,42 @@
+local mq = require('mq')
+local ForceEnd = function ()
+    mq.cmd.endlua()
+end
+
+mq.event('BrokenPole', "#*#You can't fish without a fishing pole, go buy one.#*#", ForceEnd)
+mq.event('NoBait', "#*#You can't fish without fishing bait, go buy some.#*#", ForceEnd)
+
+local KeepItem = function ()
+    while (mq.TLO.Cursor.ID() ~= 0) do
+        if (mq.TLO.Cursor.Name() == "Tattered Cloth Sandal" or mq.TLO.Cursor.Name() == "Rusty Dagger") then
+            mq.cmd.destroy()
+            mq.delay('1s')
+        else 
+            if (mq.TLO.Cursor.Name() ~= "Fish Scales") then
+                mq.cmd.echo('Caught '..mq.TLO.Cursor.Name())
+            end
+            cmd.autoinventory()
+        end
+    end
+end
+
+local CheckPole = function ()
+    if (string.find(mq.TLO.Me.Inventory('mainhand').Name(), 'The Bone Rod') ~= nil) then return end
+    mq.cmd.echo('You need to put your fishing pole in your primary hand.')
+    mq.cmd.endlua()
+end
+
+while true do
+    CheckPole()
+    mq.delay(10)
+
+    KeepItem()
+    mq.cmd.doability('Fishing')
+    mq.delay(10)
+
+    KeepItem()
+    mq.cmd.doability('Forage')
+    mq.delay(10)
+
+    mq.doevents()
+end
