5790ed6a897321b40f446d8b11e78aa9f5021989
Knightly <knightly.cavalier@gmail.com>
2021-07-09 19:23:48 -1000

    Add MQ2Main Prebuild Script - Prebuild script will check vswhere and locale and provide warnings - Hopefully this makes things more clear when errors happen - Fixes #325

---
+++ tools/build_scripts/MQ2Main_PreBuild.ps1
@@ -0,0 +1,21 @@
+# This will have to be modified when VS2022 comes out (64 bit)
+$vswherePath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+
+if (-Not (Test-Path -Path $vswherePath -PathType Leaf))
+{
+    Write-Warning "Crashpad build relies on chromium which relies on vswhere. You may have issues, could not find: $vswherePath"
+}
+else
+{
+    $vswhereResult = & $vswherePath -latest -property installationPath
+    if ([string]::IsNullOrWhiteSpace($vswhereResult))
+    {
+        Write-Warning "Crashpad build relies on chromium which relies on vswhere for your path.  vswhere did not return a path.  Make sure a current build of Visual Studio (not Preview) is installed."
+    }
+}
+
+$systemLocale = (Get-WinSystemLocale).Name
+if (-Not $systemLocale.StartsWith("en"))
+{
+    Write-Warning "You may have problems building if you have non-unicode characters in your paths.  To resolve this issue, change your system locale to English"
+}
