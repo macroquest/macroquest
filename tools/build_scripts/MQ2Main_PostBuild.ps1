a6dad65d3ff699e65cb228ef915c18b9d8c5994b
Knightly <knightly.cavalier@gmail.com>
2021-07-09 17:38:07 -1000

    Organizational Cleanup - Moved TestSuite to IntegrationTests under the lua plugin - Removed BinCopy for lua since that's handled automatically

--- tools/build_scripts/MQ2Main_PostBuild.ps1
+++ tools/build_scripts/MQ2Main_PostBuild.ps1
@@ -73,7 +73,7 @@ try {
 			Copy-LatestFile $file "$Dst\$relative" $relative
 		}
 	}
-	
+
 	# Copy crash reporter
 	Copy-LatestFile "$Src\..\contrib\vcpkg\installed\x86-windows-static\tools\crashpad_handler.exe" "$Dst\mq_crash_handler.exe" "mq_crash_handler.exe"
 }
