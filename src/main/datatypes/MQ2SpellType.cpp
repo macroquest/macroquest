23fdca3f76af23b0c8a1d8f53e44eabbc1cd00e9
Dustin Niehoff <dannuic@gmail.com>
2021-12-26 22:04:54 -0700

    Added fallback to autocast for spells with triggers

--- src/main/datatypes/MQ2SpellType.cpp
+++ src/main/datatypes/MQ2SpellType.cpp
@@ -1220,12 +1220,17 @@ bool MQ2SpellType::GetMember(MQVarPtr VarPtr, const char* Member, char* Index, M
 		Dest.Type = pSpellType;
 
 		int spaFound = GetTriggerSPA(pSpell);
-		if (!Index[0] || spaFound == 0)
+		int autoCast = pSpell->Autocast;
+		int index = !Index[0] ? -1 : GetIntFromString(Index, 0) - 1;
+
+		if (spaFound == 0 && autoCast == 0)
 			return false;
 
-		int index = GetIntFromString(Index, 0) - 1;
-		if (index < 0 || index >= pSpell->NumEffects)
-			return false;
+		if (spaFound == 0 || index < 0 || index >= pSpell->NumEffects)
+		{
+			Dest.Ptr = GetSpellByID(autoCast);
+			return Dest.Ptr != nullptr;
+		}
 
 		int base2 = GetSpellBase2(pSpell, index);
 		if (base2 == 0)
