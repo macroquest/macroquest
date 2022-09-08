d03b5314e9468a19b8c5930380d1e266ada7a3aa
Knightly <knightly.cavalier@gmail.com>
2022-09-07 20:31:47 -1000

    Update loader for dual installs

--- src/loader/MacroQuest.cpp
+++ src/loader/MacroQuest.cpp
@@ -1151,7 +1151,6 @@ int WINAPI CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR 
 	else if (!gEnableCrashpad)
 		SPDLOG_INFO("Crashpad is disabled.");
 
-#if defined(_WIN64)
 	// TODO:  Allow argument processing of passing ini file so the file can be launched from anywhere
 	std::string fullCommandLine = "";
 	bool spawnedProcess = false;
@@ -1257,7 +1256,6 @@ int WINAPI CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR 
 			exit(0);
 		}
 	}
-#endif // defined(_WIN64)
 
 	// Initialize COM
 	auto coCleanup = wil::CoInitializeEx();
