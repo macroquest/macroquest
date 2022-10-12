364e665c0894aa2fa05ba71fc96e4a46d8a896ab
Dustin Niehoff <dannuic@gmail.com>
2022-10-12 10:59:20 -0600

    Removed superfluous macro check

--- src/main/datatypes/MQ2MacroType.cpp
+++ src/main/datatypes/MQ2MacroType.cpp
@@ -313,14 +313,9 @@ bool MQ2MacroType::ToString(MQVarPtr VarPtr, char* Destination)
 
 bool MQ2MacroType::dataMacro(const char* szIndex, MQTypeVar& Ret)
 {
-	if (gRunning)
-	{
-		Ret.Ptr = 0;
-		Ret.Type = pMacroType;
-		return true;
-	}
-
-	return false;
+	Ret.Ptr = 0;
+	Ret.Type = pMacroType;
+	return true;
 }
 
 } // namespace mq::datatypes
