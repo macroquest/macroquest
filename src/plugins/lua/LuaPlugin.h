b023ea5734374fad7d9fb16d7659699678bbe2eb
Dustin Niehoff <dannuic@gmail.com>
2022-11-09 16:00:24 -0700

    Added datatypes

--- src/plugins/lua/LuaPlugin.h
+++ src/plugins/lua/LuaPlugin.h
@@ -20,10 +20,126 @@
 #include "LuaCommon.h"
 
 #include <mq/Plugin.h>
+#include <sol/sol.hpp>
 
 namespace mq::lua {
+class MQ2LuaGenericType : public MQ2Type
+{
+private:
+	std::string m_typeName;
+	ci_unordered::map<const char*, sol::function> m_memberMap;
+	sol::function m_toString;
+	sol::function m_fromData;
+	sol::function m_fromString;
+
+public:
+	MQ2LuaGenericType(std::string_view typeName, sol::function toString, sol::function fromData, sol::function fromString)
+		: m_typeName(typeName)
+		, m_toString(toString)
+		, m_fromData(fromData)
+		, m_fromString(fromString)
+		, MQ2Type(fmt::format("{}", typeName).c_str()) // not a huge fan of this, but the base class will init first, can't use m_typeName
+	{}
+
+	virtual bool GetMember(MQVarPtr VarPtr, const char* Member, char* Index, MQTypeVar& Dest) override
+	{
+		auto member_it = m_memberMap.find(Member);
+		auto ptr = VarPtr.Get<sol::object>();
+		if (member_it != m_memberMap.end() && ptr)
+		{
+			auto result = member_it->second(*ptr, Index);
+			if (result.valid() && result.return_count() > 1)
+			{
+				std::tuple<std::optional<std::string_view>, sol::object> r = result;
+				std::optional<std::string_view> typeName = std::get<0>(r);
+				sol::object typeValue = std::get<1>(r);
+				if (typeName)
+				{
+					MQ2Type* type = FindMQ2DataType(fmt::format("{}", *typeName).c_str());
+					if (type != nullptr)
+					{
+						Dest.Type = type;
+
+						// now take the value, stringify it, then use the type's fromstring to deser it
+						auto pp = sol::stack::push_pop(typeValue);
+						auto stack_val = sol::stack_object(typeValue.lua_state(), -1);
+						std::size_t len;
+						const char* val_str = lua_tolstring(stack_val.lua_state(), stack_val.stack_index(), &len);
+						lua_pop(stack_val.lua_state(), 1);
+
+						return type->FromString(Dest, val_str);
+					}
+				}
+			}
+		}
+		
+		return false;
+	}
+
+	bool ToString(MQVarPtr VarPtr, char* Destination) override
+	{
+		auto ptr = VarPtr.Get<sol::object>();
+		if (ptr)
+		{
+			auto result = m_toString(*ptr);
+			if (result.valid() && result.return_count() > 0)
+			{
+				std::optional<std::string_view> r = result;
+				fmt::format_to(Destination, "{}", r.value_or(""));
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	bool FromData(MQVarPtr& VarPtr, const MQTypeVar& Source)
+	{
+		auto ptr = Source.Get<sol::object>();
+		if (ptr)
+		{
+			auto result = m_fromData(*ptr);
+			if (result.valid() && result.return_count() > 0)
+			{
+				sol::object r = result;
+				if (r != sol::lua_nil)
+				{
+					VarPtr.Set(r);
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	bool FromString(MQVarPtr& VarPtr, const char* Source)
+	{
+		auto result = m_fromString(Source);
+		if (result.valid() && result.return_count() > 0)
+		{
+			sol::object r = result;
+			if (r != sol::lua_nil)
+			{
+				VarPtr.Set(r);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	void AddMember(const char* memberName, sol::function evaluate)
+	{
+		m_memberMap.emplace(memberName, evaluate);
+	}
+};
+
 class LuaPlugin
 {
+	// TODO: need factory method for plugins
+	// TODO: need TLO methods
+	// TODO: move some of this into an implementation file
 public:
 	enum class Callback
 	{
@@ -106,45 +222,7 @@ public:
 private:
 	std::unordered_map<Callback, std::vector<sol::function>> m_callbacks;
 
-private:
-	// This is a hack because you can't capture in a function pointer, but we need to capture the lua function
-	struct FPtr
-	{
-		template <typename T>
-		static void func_ptr_exec(PlayerClient* Player, char* Buffer)
-		{
-			return (void)(*(T*)fn<T>())(Player, Buffer);
-		}
-
-		template <typename T>
-		static fEQCommand ptr(T& t)
-		{
-			fn<T>(&t);
-			return func_ptr_exec<T>;
-		}
-
-		template <typename T>
-		static void* fn(void* new_fn = nullptr)
-		{
-			static void* fn;
-			if (new_fn != nullptr) fn = new_fn;
-			return fn;
-		}
-	};
-
 public:
-	static fEQCommand CreateCommand(sol::function func)
-	{
-		auto ret_func = [&func](PlayerClient*, char* Buffer) -> void
-		{
-			// TODO: Does Buffer include the command? If so, drop the first arg
-			auto args = tokenize_args(Buffer);
-			func(sol::as_args(args)); // TODO: should probably do some action on the result, and make sure we don't crash if the command fails
-		};
-
-		return FPtr::ptr(ret_func);
-	}
-
 	void RegisterCommand(const std::string& name, sol::function func)
 	{
 		if (IsCommand(name.c_str()))
@@ -158,7 +236,12 @@ public:
 		else
 		{
 			m_commands.emplace(name, func);
-			AddCommand(name.c_str(), CreateCommand(func)); // TODO: we might want to pass the optional booleans here
+			AddFunction(name.c_str(), [&func](PlayerClient*, char* Buffer) -> void
+				{
+					// TODO: Does Buffer include the command? If so, drop the first arg
+					auto args = tokenize_args(Buffer);
+					func(sol::as_args(args)); // TODO: should probably do some action on the result, and make sure we don't crash if the command fails
+				}); // TODO: we might want to pass the optional booleans here
 		}
 	}
 
@@ -174,6 +257,40 @@ public:
 
 private:
 	ci_unordered::map<std::string, sol::function> m_commands;
+
+public:
+	void RegisterDatatype(const std::string& name, sol::table members, sol::function toString, sol::function fromData, sol::function fromString)
+	{
+		if (FindMQ2DataType(name.c_str()) != nullptr)
+		{
+			LuaError("Cannot create datatype %s, already a datatype in MQ.", name.c_str());
+		}
+		else if (name.empty())
+		{
+			LuaError("Cannot create datatype %s, not a valid name.", name.c_str());
+		}
+		else
+		{
+			auto [it, _] = m_dataTypes.emplace(name, std::make_unique<MQ2LuaGenericType>(name, toString, fromData, fromString));
+			for (const auto& member : members)
+			{
+				auto maybe_name = member.first.as<std::optional<std::string_view>>();
+				auto maybe_val = member.second.as<std::optional<sol::function>>();
+				if (maybe_name && maybe_val)
+				{
+					it->second->AddMember(fmt::format("{}", *maybe_name).c_str(), *maybe_val);
+				}
+			}
+		}
+	}
+
+	void UnregisterDatatypes()
+	{
+		m_dataTypes.clear();
+	}
+
+private:
+	ci_unordered::map<std::string, std::unique_ptr<MQ2LuaGenericType>> m_dataTypes;
 	
 };
 } // namespace mq::lua
