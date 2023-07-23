33fc514dd4189a58b57bec8b1da30dc3f1b04a84
John <king.johnf@gmail.com>
2023-07-23 07:53:47 -0400

    adds lua_getAllGroundItems method

--- src/plugins/lua/bindings/lua_MQBindings.cpp
+++ src/plugins/lua/bindings/lua_MQBindings.cpp
@@ -256,6 +256,26 @@ static sol::table lua_getFilteredSpawns(sol::this_state L, std::optional<sol::fu
 	return table;
 }
 
+static sol::table lua_getAllGroundItems(sol::this_state L)
+{
+    auto table = sol::state_view(L).create_table();
+
+    if (pItemList)
+    {
+        auto pGroundItem = pItemList->Top;
+        while (pGroundItem != nullptr)
+        {
+            auto groundTypeVar = datatypes::MQ2GroundType::MakeTypeVar(MQGroundSpawn(pGroundItem));
+            auto lua_ground = lua_MQTypeVar(groundTypeVar);
+            table.add(std::move(lua_ground));
+
+            pGroundItem = pGroundItem->pNext;
+        }
+    }
+
+    return table;
+}
+
 static sol::table lua_getFilteredGroundItems(sol::this_state L, std::optional<sol::function> predicate)
 {
 	auto table = sol::state_view(L).create_table();
