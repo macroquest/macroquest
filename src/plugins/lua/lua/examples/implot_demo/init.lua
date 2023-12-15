44319c6d085c7481d34b65d8a7ba78c2c4561128
brainiac <brainiac2k@gmail.com>
2023-12-14 19:26:46 -0800

    Update ImPlot demo

--- src/plugins/lua/lua/examples/implot_demo/init.lua
+++ src/plugins/lua/lua/examples/implot_demo/init.lua
@@ -77,6 +77,10 @@ function ImPlotDemo:ShowDemoWindow(open)
             self:DemoHeader('Line Plots', self.Demo_LinePlots)
             self:DemoHeader("Filled Line Plots", self.Demo_FilledLinePlots);
             self:DemoHeader("Shaded Plots##", self.Demo_ShadedPlots)
+            self:DemoHeader("Scatter Plots", self.Demo_ScatterPlots)
+            self:DemoHeader("Stairstep Plots", self.Demo_StairstepPlots)
+            self:DemoHeader("Bar Plots", self.Demo_BarPlots)
+            self:DemoHeader("Bar Groups", self.Demo_BarGroups)
 
             imgui.EndTabItem()
         end
@@ -248,6 +252,128 @@ function ImPlotDemo:Demo_ShadedPlots()
         implot.EndPlot()
     end
 end
+
+local Demo_ScatterPlots_Data = {
+    xs1 = {},
+    ys1 = {},
+    xs2 = {},
+    ys2 = {},
+}
+
+function ImPlotDemo:Demo_ScatterPlots()
+    local data = Demo_ScatterPlots_Data
+    math.randomseed(0)
+
+    for i = 0, 99 do
+        data.xs1[i] = i * 0.01
+        data.ys1[i] = data.xs1[i] + 0.1 * math.random()
+    end
+
+    for i = 0, 49 do
+        data.xs2[i] = .25 + 0.2 * math.random()
+        data.ys2[i] = .75 + 0.2 * math.random()
+    end
+
+    if implot.BeginPlot("Scatter Plot") then
+        implot.PlotScatter("Data 1", data.xs1, data.ys1)
+        implot.PushStyleVar(ImPlotStyleVar.FillAlpha, 0.25)
+        implot.SetNextMarkerStyle(ImPlotMarker.Square, 6, implot.GetColormapColor(1), IMPLOT_AUTO, implot.GetColormapColor(1))
+        implot.PlotScatter("Data 2", data.xs2, data.ys2)
+        implot.PopStyleVar()
+        implot.EndPlot()
+    end
+end
+
+local Demo_StairstepPlots_Data = {
+    ys1 = {},
+    ys2 = {},
+    flags = 0,
+}
+
+function ImPlotDemo:Demo_StairstepPlots()
+    local data = Demo_StairstepPlots_Data
+
+    for i = 0, 20 do
+        data.ys1[i] = 0.75 + 0.2 * math.sin(10 * i * 0.05)
+        data.ys2[i] = 0.25 + 0.2 * math.sin(10 * i * 0.05)
+    end
+
+    data.flags = imgui.CheckboxFlags("ImPlotStairsFlags.Shaded", data.flags, ImPlotStairsFlags.Shaded)
+    if implot.BeginPlot("Stairstep Plot") then
+        implot.SetupAxes("x", "f(x)")
+        implot.SetupAxesLimits(0, 1, 0, 1)
+        
+        implot.PushStyleColor(ImPlotCol.Line, ImVec4(0.5, 0.5, 0.5, 1.0))
+        implot.PlotLine("##1", data.ys1, 0.05)
+        implot.PlotLine("##2", data.ys2, 0.05)
+        implot.PopStyleColor()
+
+        implot.SetNextMarkerStyle(ImPlotMarker.Circle)
+        implot.SetNextFillStyle(IMPLOT_AUTO_COL, 0.25)
+        implot.PlotStairs("Post Step (default)", data.ys1, 0.05, 0, data.flags)
+        implot.SetNextMarkerStyle(ImPlotMarker.Circle)
+        implot.SetNextFillStyle(IMPLOT_AUTO_COL, 0.25)
+        implot.PlotStairs("Pre Step", data.ys2, 0.05, 0, bit32.bor(data.flags, ImPlotStairsFlags.PreStep))
+
+        implot.EndPlot()
+    end
+end
+
+local Demo_BarPlots_Data = {
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+}
+
+function ImPlotDemo:Demo_BarPlots()
+    local data = Demo_BarPlots_Data
+    
+    if implot.BeginPlot("Bar Plot") then
+        implot.PlotBars("Vertical", data, 0.7, 1)
+        implot.PlotBars("Horizontal", data, 0.4, 1, ImPlotBarsFlags.Horizontal)
+        implot.EndPlot()
+    end
+end
+
+local Demo_BarGroups_Data = {
+    data = {83, 67, 23, 89, 83, 78, 91, 82, 85, 90,  -- midterm
+            80, 62, 56, 99, 55, 78, 88, 78, 90, 100, -- final
+            80, 69, 52, 92, 72, 78, 75, 76, 89, 95}, -- course
+    ilabels = {"Midterm Exam", "Final Exam", "Course Grade"},
+    glabels = {"S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10"},
+    positions = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+    items = 3,
+    groups = 10,
+    size = 0.67,
+    flags = 0,
+    horz = false
+}
+
+function ImPlotDemo:Demo_BarGroups()
+    local data = Demo_BarGroups_Data
+
+    data.flags = imgui.CheckboxFlags("Stacked", data.flags, ImPlotBarGroupsFlags.Stacked)
+    imgui.SameLine()
+    data.horz = imgui.Checkbox("Horizontal", data.horz)
+
+    data.items = imgui.SliderInt("Items", data.items, 1, 3)
+    data.size = imgui.SliderFloat("Size", data.size, 0, 1)
+
+    if implot.BeginPlot("Bar Group") then
+        implot.SetupLegend(ImPlotLocation.East, ImPlotLegendFlags.Outside)
+
+        if data.horz then
+            implot.SetupAxes("Score", "Student", ImPlotAxisFlags.AutoFit, ImPlotAxisFlags.AutoFit)
+            implot.SetupAxisTicks(ImAxis.Y1, data.positions, data.glabels)
+            implot.PlotBarGroups(data.ilabels, data.data, data.items, data.groups, data.size, 0, bit32.bor(data.flags, ImPlotBarGroupsFlags.Horizontal))
+        else
+            implot.SetupAxes("Student", "Score", ImPlotAxisFlags.AutoFit, ImPlotAxisFlags.AutoFit)
+            implot.SetupAxisTicks(ImAxis.X1, data.positions, data.glabels)
+            implot.PlotBarGroups(data.ilabels, data.data, data.items, data.groups, data.size, 0, data.flags)
+        end
+
+        implot.EndPlot()
+    end
+end
+
 
 
 --
