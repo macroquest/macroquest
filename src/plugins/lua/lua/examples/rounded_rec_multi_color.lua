f2fc8452fc0fbf0e11b54a82607ea6302f3412a7
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2026-02-27 19:10:54 -0700

    demo adjustment after revert

--- src/plugins/lua/lua/examples/rounded_rec_multi_color.lua
+++ src/plugins/lua/lua/examples/rounded_rec_multi_color.lua
@@ -48,7 +48,7 @@ local function RenderWin()
         rectSize.height = ImGui.SliderFloat("Rect Height", rectSize.height, 4, 400)
 
         -- The rounding size should not be larger than half of the smaller dimension of the rectangle, otherwise it will look strange. So we calculate the larger dimension and use that to limit the rounding size.
-        local tmpLarger = rectSize.width > rectSize.height and rectSize.width or rectSize.height
+        local tmpLarger = rectSize.width < rectSize.height and rectSize.width or rectSize.height
         if roundingSize > tmpLarger / 2 then roundingSize = tmpLarger / 2 end
         roundingSize = ImGui.SliderFloat("Rounding Size", roundingSize, 0, tmpLarger / 2)
 
@@ -109,66 +109,6 @@ local function RenderWin()
             top_left = ImGui.GetCursorScreenPosVec()
             bottom_right = ImVec2(top_left.x + rectSize.width, top_left.y + rectSize.height)
             startPos = ImGui.GetCursorPosVec()
-        end
-
-
-        if ImGui.CollapsingHeader("AddRectFilledMultiColorRoundedAnimated") then
-            ImGui.TextWrapped("This demonstrates the animated version of Rounded Multicolor Rect. \nChange the colors and see the animation. ")
-
-            ImGui.Spacing()
-
-            ImGui.TextWrapped(
-                "This uses ImAnim color blending for smother transitions between color changes. \nThe animation is tracked using the provided ID, so changing the colors will animate from the previous colors to the new ones.  \nIf you change the colors quickly, it will blend from the current color to the new color, creating a smooth transition effect.")
-
-            local top_left = ImGui.GetCursorScreenPosVec()
-            local bottom_right = ImVec2(top_left.x + rectSize.width, top_left.y + rectSize.height)
-            local startPos = ImGui.GetCursorPosVec()
-
-            dl:AddRectFilledMultiColorRoundedAnimated(
-                1,                                     -- integer ID for animation tracking
-                top_left,                              -- top left corner
-                bottom_right,                          -- bottom right corner
-                ImGui.ColorConvertFloat4ToU32(c_blue), -- UL color
-                ImGui.ColorConvertFloat4ToU32(c_red),  -- UR color
-                ImGui.ColorConvertFloat4ToU32(c_red),  -- BR color
-                ImGui.ColorConvertFloat4ToU32(c_blue), -- BL color
-                roundingSize,
-                drawFlags[selectedFlag]
-            )
-
-
-            top_left.x = top_left.x + rectSize.width + 10
-            bottom_right = ImVec2(top_left.x + rectSize.width, top_left.y + rectSize.height)
-
-            dl:AddRectFilledMultiColorRoundedAnimated(
-                2, -- "ID_Value", -- integer ID for animation tracking
-                top_left,
-                bottom_right,
-                ImGui.ColorConvertFloat4ToU32(c_blue), -- UL color
-                ImGui.ColorConvertFloat4ToU32(c_blue), -- UR color
-                ImGui.ColorConvertFloat4ToU32(c_red),  -- BR color
-                ImGui.ColorConvertFloat4ToU32(c_red),  -- BL color
-                roundingSize,
-                drawFlags[selectedFlag]
-            )
-
-            top_left.x = top_left.x + rectSize.width + 10
-            bottom_right = ImVec2(top_left.x + rectSize.width, top_left.y + rectSize.height)
-
-            dl:AddRectFilledMultiColorRoundedAnimated(
-                3, -- "ID_Value", -- integer ID for animation tracking
-                top_left,
-                bottom_right,
-                ImGui.ColorConvertFloat4ToU32(c_blue), -- color for top left
-                ImGui.ColorConvertFloat4ToU32(c_red),  -- color for top right
-                ImGui.ColorConvertFloat4ToU32(c_blue), -- color for bottom right
-                ImGui.ColorConvertFloat4ToU32(c_red),  -- color for bottom left
-                roundingSize,
-                drawFlags[selectedFlag]
-            )
-
-            ImGui.SetCursorPos(startPos.x, startPos.y + rectSize.height + 2)
-            ImGui.Dummy(0, 0)
         end
     end
     ImGui.End()
