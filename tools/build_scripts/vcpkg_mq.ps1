a0e11c4d42eaece18d4816dfc46ed1d30e32562c
Knightly <knightly.cavalier@gmail.com>
2026-04-06 21:40:36 -1000

    vcpkg - add triplet overlay

--- tools/build_scripts/vcpkg_mq.ps1
+++ tools/build_scripts/vcpkg_mq.ps1
@@ -40,6 +40,7 @@ $env:VCPKG_FORCE_DOWNLOADED_BINARIES = "1"
 
 $vcpkg_root = "$MQRoot\contrib\vcpkg"
 $vcpkg_overlay_ports = "$MQRoot\contrib\vcpkg-overlays\msbuild"
+$vcpkg_overlay_triplets = "$MQRoot\contrib\vcpkg-overlays\triplets"
 $vcpkg_triplet = "$Platform-windows-static"
 $vcpkg_mq_file = "vcpkg_mq.txt"
 $vcpkg_mq_file_platform = "vcpkg_mq_$Platform.txt"
@@ -109,14 +110,18 @@ if (-Not (Test-Path -Path "$vcpkg_root" -PathType Container))
 }
 
 # The vcpkg_mq file might override this, but at least check the defaults
-$tripletFile = "$vcpkg_root\triplets\$vcpkg_triplet.cmake"
+$tripletFile = "$vcpkg_overlay_triplets\$vcpkg_triplet.cmake"
 if (-Not (Test-Path -Path "$tripletFile" -PathType Leaf))
 {
-    $tripletFile = "$vcpkg_root\triplets\community\$vcpkg_triplet.cmake"
+    $tripletFile = "$vcpkg_root\triplets\$vcpkg_triplet.cmake"
     if (-Not (Test-Path -Path "$tripletFile" -PathType Leaf))
     {
-        Write-Error "VCPKG Default Triplet not found: $vcpkg_triplet"
-        exit 1
+        $tripletFile = "$vcpkg_root\triplets\community\$vcpkg_triplet.cmake"
+        if (-Not (Test-Path -Path "$tripletFile" -PathType Leaf))
+        {
+            Write-Error "VCPKG Default Triplet not found: $vcpkg_triplet"
+            exit 1
+        }
     }
 }
 
@@ -271,7 +276,7 @@ foreach ($triplet in $tripletsToCheck) {
 
 # If we did a bootstrap and we already had installed packages
 if ($performBootstrap -And $vcpkgTable.Count -ne 0) {
-    & ./vcpkg.exe upgrade --no-dry-run --overlay-ports="$vcpkg_overlay_ports"
+    & ./vcpkg.exe upgrade --no-dry-run --overlay-ports="$vcpkg_overlay_ports" --overlay-triplets="$vcpkg_overlay_triplets"
     if ($LASTEXITCODE -ne 0) {
         Write-Warning "vcpkg upgrade failed - your vcpkg installation may need to be manually fixed. Save this log before trying again."
         # if the upgrade failed, the bootstrap file should show accordingly
@@ -353,7 +358,7 @@ foreach ($file in $vcpkg_file_list) {
 
 if ($vcpkgInstallTable.Count -ne 0) {
     RunPreCheck
-    $vcpkg_command = "install --x-wait-for-lock --overlay-ports=`"$vcpkg_overlay_ports`""
+    $vcpkg_command = "install --x-wait-for-lock --overlay-ports=`"$vcpkg_overlay_ports`" --overlay-triplets=`"$vcpkg_overlay_triplets`""
     foreach ($triplet in $vcpkgInstallTable.GetEnumerator()) {
         if ($triplet.Value.Count -ne 0) {
             foreach ($node in $triplet.Value.GetEnumerator()) {
